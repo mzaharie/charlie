10a06416aa1e9dc9ae08ac3117434423d9fef45f
Marius <mzaharie@pitechnologies.ro>
2013-10-31 11:16:04 +0200

    ajax - add to cart button

--- src/Bookshop/BookshopBundle/Resources/public/js/ajax.js
+++ src/Bookshop/BookshopBundle/Resources/public/js/ajax.js
@@ -1,22 +1,34 @@
-function loadCart(url){
+function loadCart(url) {
     var img = $('<img />');
     img.attr('id', 'cart-loading');
     img.attr('src', '/bundles/bookshopbookshop/css/images/cartLoading.gif');
     $('#cart').add($('<img />').attr({'id': 'cart-loading', 'src': '/bundles/bookshopbookshop/css/images/cartLoading.gif'}));
     img.appendTo('#cart');
-    $('#cart').load(url,function( response, status, xhr ) {
+    $('#cart').load(url, function(response, status, xhr) {
         $('#cart>img').hide();
     });
-    
-    
+
+
 }
-function loadMessages(url){
+function loadMessages(url) {
     $('#messages').load(url);
 }
-function deleteFromCart(url,msg_path,cart_path){
+function deleteFromCart(url, msg_path, cart_path) {
     $.ajax(url).done(function() {
         loadMessages(msg_path);
         loadCart(cart_path);
+    });
+}
+
+function add(url, qty, prodId, msgs_url, cart_url) {
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: {quantity: qty, productid: prodId}
+    })
+            .done(function() {
+        loadMessages(msgs_url);
+        loadCart(cart_url);
     });
 }
 
